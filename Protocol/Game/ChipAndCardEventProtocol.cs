0cde4f74036ee785cab8f2b1b39309830062dda6
FanJae <hjjang45@gmail.com>
2026-07-22 21:56:49 +0900

    [Refactor] 게임 이벤트 프로토콜 파서 분리
    - 게임 이벤트를 기본 흐름, 칩 및 카드, 베팅 이벤트로 분리
    - 게임 시작, 턴, 결과 및 초기화 이벤트 파서 추가
    - 내 칩, 상대 칩, 카드 및 딜러 칩 갱신 이벤트 파서 추가
    - 내 베팅, 상대 베팅, 베팅 불가 및 양면 베팅 이벤트 파서 추가
    - 카드 면과 카드 값을 파싱하고 이미지 파일명을 생성하는 CardProtocol 추가
    - ChattingRoom의 긴 문자열 조건 분기를 전용 파서 기반 처리로 변경

---
+++ Protocol/Game/ChipAndCardEventProtocol.cs
@@ -0,0 +1,84 @@
+﻿using TwofacedPoker_Client.Common;
+
+namespace TwofacedPoker_Client.Protocol.Game
+{
+    public enum ChipAndCardEventType
+    {
+        Unknown,
+        MyChipUpdate,
+        OpponentChipUpdate,
+        MyCardUpdate,
+        OpponentCardUpdate,
+        DealerChipUpdate,
+        OpponentCardPrint
+    }
+
+    public class ChipAndCardEventMessage
+    {
+        public ChipAndCardEventType Type { get; }
+        public string Value { get; }
+
+        public ChipAndCardEventMessage(ChipAndCardEventType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+
+    public static class ChipAndCardEventParser
+    {
+        public static ChipAndCardEventMessage Parse(string message)
+        {
+            // 각 주체의 칩 변동 및 카드 업데이트 상태를 문자열 Prefix로 비교
+            string prefix = Constants.MY + Constants.CHIP_UPDATE;
+
+            if (message.StartsWith(prefix))
+            {
+                string value = message.Substring(prefix.Length);
+                return new ChipAndCardEventMessage(ChipAndCardEventType.MyChipUpdate, value);
+            }
+
+            prefix = Constants.OTHER + Constants.CHIP_UPDATE;
+
+            if (message.StartsWith(prefix))
+            {
+                string value = message.Substring(prefix.Length);
+                return new ChipAndCardEventMessage(ChipAndCardEventType.OpponentChipUpdate, value);
+            }
+
+            prefix = Constants.MY + Constants.CARD_UPDATE;
+
+            if (message.StartsWith(prefix))
+            {
+                string value = message.Substring(prefix.Length);
+                return new ChipAndCardEventMessage(ChipAndCardEventType.MyCardUpdate, value);
+            }
+
+            prefix = Constants.OTHER + Constants.CARD_UPDATE;
+
+            if (message.StartsWith(prefix))
+            {
+                string value = message.Substring(prefix.Length);
+                return new ChipAndCardEventMessage(ChipAndCardEventType.OpponentCardUpdate, value);
+            }
+
+            prefix = Constants.DEALER + Constants.CHIP_UPDATE;
+
+            if (message.StartsWith(prefix))
+            {
+                string value = message.Substring(prefix.Length);
+                return new ChipAndCardEventMessage(ChipAndCardEventType.DealerChipUpdate, value);
+            }
+
+            prefix = Constants.OTHER + Constants.PRINT;
+
+            if (message.StartsWith(prefix))
+            {
+                string value = message.Substring(prefix.Length);
+                return new ChipAndCardEventMessage(ChipAndCardEventType.OpponentCardPrint, value);
+            }
+
+            return new ChipAndCardEventMessage(ChipAndCardEventType.Unknown, message);
+        }
+    }
+}
