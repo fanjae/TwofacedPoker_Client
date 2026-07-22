eceb2983f376969af56b37a847b0e743186c8d79
FanJae <hjjang45@gmail.com>
2026-07-22 22:06:04 +0900

    [Refactor] 베팅 타입 변환 및 검증 로직 분리
    - 베팅 종류를 BetType enum으로 정의
    - 베팅 패킷 문자열과 BetType 간 변환 로직을 BetPacketConverter로 분리
    - 앞면, 뒷면 및 양면 베팅 패킷 파싱 처리 통합
    - 입력 칩 개수와 베팅 타입 선택 여부 검증
    - 내 보유 칩과 상대방의 수용 가능 칩 한도 검증
    - 상대 베팅 금액을 기준으로 최소 콜 금액 검증
    - ChattingRoom에 포함되어 있던 베팅 조건문과 오류 메시지 생성 책임 분리

---
+++ Game/Betting/BetPacketConverter.cs
@@ -0,0 +1,50 @@
+﻿using TwofacedPoker_Client.Common;
+
+namespace TwofacedPoker_Client.Game.Betting
+{
+    internal static class BetPacketConverter
+    {
+        public static bool TryParse(string betMessage, out BetType betType, out int betCount)
+        {
+            betType = BetType.None;
+            betCount = 0;
+
+            string betCountText;
+
+            // 패킷의 접두사를 확인하여 베팅 타입과 칩 개수 문자열 분리
+            if (betMessage.StartsWith(Constants.FRONT))
+            {
+                betType = BetType.Front;
+                betCountText = betMessage.Substring(Constants.FRONT.Length);
+            }
+            else if (betMessage.StartsWith(Constants.BACK))
+            {
+                betType = BetType.Back;
+                betCountText = betMessage.Substring(Constants.BACK.Length);
+            }
+            else if (betMessage.StartsWith(Constants.BOTH))
+            {
+                betType = BetType.Both;
+                betCountText = betMessage.Substring(Constants.BOTH.Length);
+            }
+            else
+            {
+                return false;
+            }
+
+            // 분리한 문자열을 숫자로 반환
+            return int.TryParse(betCountText, out betCount);
+        }
+
+        public static string ToPacketText(BetType betType)
+        {
+            return betType switch
+            {
+                BetType.Front => Constants.FRONT,
+                BetType.Both => Constants.BOTH,
+                BetType.Back => Constants.BACK,
+                _ => string.Empty
+            };
+        }
+    }
+}
